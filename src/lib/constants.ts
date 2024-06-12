079d7e15b9cd6c256cea2fc4012be800ec19cffe
wesleyara <wes1gom@gmail.com>
2024-06-11 23:32:37 -0300

    feat: adding endpoints routes and export file

---
+++ src/lib/constants.ts
@@ -0,0 +1,11 @@
+export const endpoints = {
+  register: "/register", // POST { "name": "Example", "email": "example@test.com", "password": "123456" }
+  login: "/login", // POST { "email": "example@test.com", "password": "123456" }
+  me: "/me", // GET
+  cards: "/cards", // GET ?rpp=10&page=1
+  addCards: "/me/cards", // POST { "cardIds": [ "123456" ] }
+  userCards: "/me/cards", // GET
+  addTrades: "/trades", // POST { "cards": [ { "cardId": "card-uuid", "type": "OFFERING" }, { "cardId": "new-card-uuid", "type": "RECEIVING" } ] }
+  trades: "/trades", // GET ?rpp=10&page=1
+  deleteTrade: "/trades", // DELETE /:id
+};
