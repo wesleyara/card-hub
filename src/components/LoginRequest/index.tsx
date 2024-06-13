c6719fcdaf098b137281182c8edbc7cf8b247ad9
wesleyara <wes1gom@gmail.com>
2024-06-13 11:51:59 -0300

    fix: ajust button width

--- src/components/LoginRequest/index.tsx
+++ src/components/LoginRequest/index.tsx
@@ -4,11 +4,14 @@ export const LoginRequest = () => {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col items-center gap-5">
       <h2 className="text-center">
         Por favor realize o login para visualizar essa página!
       </h2>
-      <button className="btn-secondary" onClick={() => router.push("/login")}>
+      <button
+        className="btn-secondary max-w-[400px]"
+        onClick={() => router.push("/login")}
+      >
         Realizar login
       </button>
     </div>
